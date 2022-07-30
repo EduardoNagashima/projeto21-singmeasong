8deb501c1448d4ec9302b5cdc748a9667b04caf2
eduardoNagashima <eduyudji@gmail.com>
2022-07-30 20:49:58 -0300

    some tests made

--- back-end/tests/unit/recommendationsService.test.ts
+++ back-end/tests/unit/recommendationsService.test.ts
@@ -15,6 +15,20 @@ describe("recommendation", () => {
         expect(recommendationRepository.create).toBeCalled();
     })
 
+    it("create new FAIL", async () => {
+        try {
+            jest.spyOn(recommendationRepository, "create").mockImplementationOnce((): any => { });
+            const recommentation = {
+                name: 'JoJo Op 5',
+                youtubeLink: 'https://www.youtube.com/watch?v=lKDwS2hnQ24'
+            }
+            jest.spyOn(recommendationRepository, "findByName").mockImplementationOnce((): any => { return recommentation });
+            await recommendationService.insert(recommentation);
+        } catch (e) {
+            expect(e).toEqual({ message: "Recommendations names must be unique", type: "conflict" });
+        }
+    })
+
     it('vote', async () => {
         jest.spyOn(recommendationRepository, "updateScore").mockImplementationOnce((): any => { });
         jest.spyOn(recommendationRepository, "find").mockImplementationOnce((): any => { return 1 });
@@ -34,5 +48,32 @@ describe("recommendation", () => {
         expect(recommendationRepository.updateScore).toBeCalled();
     });
 
+    it('get recommendation by Id', async () => {
+        jest.spyOn(recommendationRepository, "find").mockImplementationOnce((): any => { return 1 });
+        const recommendation = await recommendationService.getById(1);
+
+        expect(recommendationRepository.find).toBeCalled();
+    });
+
+    it('get recommendation by Id FAIL', async () => {
+        try {
+            jest.spyOn(recommendationRepository, "find").mockImplementationOnce((): any => { false });
+            await recommendationService.getById(1);
+        } catch (e) {
+            expect(e).toEqual({ type: 'not_found', message: '' })
+        }
+    });
+
+    it('get all recommentation', async () => {
+        jest.spyOn(recommendationRepository, "findAll").mockImplementationOnce((): any => { [] });
+        await recommendationService.get();
+        expect(recommendationRepository.findAll).toBeCalled();
+    })
+
+    it('get all recommentation', async () => {
+        jest.spyOn(recommendationRepository, "getAmountByScore").mockImplementationOnce((): any => { [] });
+        await recommendationService.getTop(10);
+        expect(recommendationRepository.getAmountByScore).toBeCalled();
+    })
 
 })
